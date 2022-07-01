5e411ca0aeeb7f32a167ddea446dca1f5ee0f501
Marco Mignacco <marcomignacco03@gmail.com>
2022-07-01 20:48:14 -0300

    added template for the tp

---
+++ src/List/LinkedList.java
@@ -0,0 +1,41 @@
+package List;
+
+public class LinkedList implements List {
+
+    @Override
+    public boolean insert(String value) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean insert(String value, int pos) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean delete(String value) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Node searchElement(String value) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Node modifyElementByConsole(String toModifyValue) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void printElements() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
