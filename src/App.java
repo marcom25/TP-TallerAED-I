ecc3eb4b1f5671f81851e556b74a8bd323ff1a47
Marco Mignacco <marcomignacco03@gmail.com>
2022-07-01 21:17:17 -0300

    MDOAMODWOAWOPD

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
+import List.LinkedList;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+
+        list.insert("hola");
     }
 }
--- src/List/LinkedList.java
+++ src/List/LinkedList.java
@@ -1,40 +1,48 @@
 package List;
 
+
+
 public class LinkedList implements List {
+
+    private Node list;
 
     @Override
     public boolean insert(String value) {
-        // TODO Auto-generated method stub
-        return false;
+        Node newNode = new Node(value);
+        newNode.setNext(list);
+        list = newNode;
+
+        return true;
     }
 
     @Override
     public boolean insert(String value, int pos) {
-        // TODO Auto-generated method stub
+        
+
         return false;
     }
 
     @Override
     public boolean delete(String value) {
-        // TODO Auto-generated method stub
+        
         return false;
     }
 
     @Override
     public Node searchElement(String value) {
-        // TODO Auto-generated method stub
+        
         return null;
     }
 
     @Override
     public Node modifyElementByConsole(String toModifyValue) {
-        // TODO Auto-generated method stub
+        
         return null;
     }
 
     @Override
     public void printElements() {
-        // TODO Auto-generated method stub
+        
         
     }
     
